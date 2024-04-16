f9eb9f1420d2637202cf5a543b14121dc1c3c488
Michelle Wetzler <1840448+wetzler@users.noreply.github.com>
2024-04-16 14:38:50 -0700

    update to use .env

--- receive_and_store_message.py
+++ receive_and_store_message.py
@@ -7,31 +7,37 @@ import generative_ai
 import send_response
 
 # this stuff needs to be there for this to run on PythonAnywhere
-# from dotenv import load_dotenv
-# project_folder = os.path.expanduser('~/.')  # adjust as appropriate
-# load_dotenv(os.path.join(project_folder, '.env'))
+from dotenv import load_dotenv
+project_folder = os.path.expanduser('~/.')  # adjust as appropriate
+load_dotenv(os.path.join(project_folder, '.env'))
 
 # Get Airtable parameters from environment variables
-base_key = os.getenv('AIRTABLE_BASE_KEY')
-table_name = os.getenv('AIRTABLE_TABLE_NAME')
-access_token = os.getenv('AIRTABLE_PERSONAL_ACCESS_TOKEN')
+base_key = os.environ.get('AIRTABLE_BASE_KEY')
+table_name = os.environ.get('AIRTABLE_TABLE_NAME')
+access_token = os.environ.get('AIRTABLE_PERSONAL_ACCESS_TOKEN')
 
-app = Flask(__name__) 
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Hello, World!"
 
 @app.route('/sms', methods=['POST'])
 def sms():
+    print(table_name)
     # Get the text message details
     from_number = request.form['From']
     to_number = request.form['To']
     message_body = request.form['Body']
     twilio_message_sid  = request.form['MessageSid']
     print("New message from "+ from_number + ": "+ message_body)
-    # Log the message
-    storage.store_message(from_number, to_number, message_body,"incoming user message",twilio_message_sid)
 
-    # Get some information about this conversation so we can figure out what to do next 
-    # Define the filter formula to find messages to or from this number 
-    filter_formula = f"(OR(from_number='{from_number}', to_number='{from_number}'))"   
+    # Log the message
+    response = storage.store_message(from_number, to_number, message_body,"incoming user message",twilio_message_sid)
+    print(response)
+    # Get some information about this conversation so we can figure out what to do next
+    # Define the filter formula to find messages to or from this number
+    filter_formula = f"(OR(from_number='{from_number}', to_number='{from_number}'))"
     # to-do: only get the messages from the last 24 hours instead of all of them
 
     # URL-encode the filter formula
@@ -40,12 +46,12 @@ def sms():
     # Define the URL to make the request to the database
     url = f"https://api.airtable.com/v0/{base_key}/{table_name}?filterByFormula={filter_formula}"
 
-    # Define the headers for the request 
+    # Define the headers for the request
     headers = {
         "Authorization": f"Bearer {access_token}"
     }
 
-    # Make the GET request for the conversation data 
+    # Make the GET request for the conversation data
     recentmessages = requests.get(url, headers=headers)
     # Parse the JSON response
     data = recentmessages.json()
