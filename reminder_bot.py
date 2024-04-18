d0141838c39572dd5fcb3566192c81d2c51f3905
Michelle Wetzler <1840448+wetzler@users.noreply.github.com>
2024-04-18 15:23:28 -0700

    reminder bot now queries db for users list

--- reminder_bot.py
+++ reminder_bot.py
@@ -7,19 +7,34 @@ import storage
 
 # this stuff needs to be there for this to run on PythonAnywhere
 from dotenv import load_dotenv
-project_folder = os.path.expanduser('~/.')  # adjust as appropriate
+project_folder = os.path.expanduser('~/.') 
 load_dotenv(os.path.join(project_folder, '.env'))
 
 # Twilio credentials
 account_sid = os.environ.get('TWILIO_ACCOUNT_SID')
 auth_token = os.environ.get('TWILIO_AUTH_TOKEN')
 twilio_number = os.environ.get('TWILIO_PHONE_NUMBER')
-user_number = os.environ.get('PERSONAL_PHONE_NUMBER')
 
+# Get list of numbers that need reminders
+url = f"https://api.airtable.com/v0/{base_key}/Users"  # Define the URL to make the request to the database
+
+headers = {   # Define the headers for the request
+    "Authorization": f"Bearer {access_token}"
+}
+
+# Make the GET request for the users data
+response = requests.get(url, headers=headers)
+data = response.json()
+users = data.get('records', [])
+print("Found "+str(len(records))+" users.")
+
+# Get all user_number values
+user_numbers = [record['fields']['user_number'] for record in records]
+ 
 # Initialize Twilio client
 client = Client(account_sid, auth_token)
 
-def send_reminder():
+def send_reminder(user_number):
     text_reminder = "hi, it's gratitude bot. what are you feeling grateful for today? 🌟"
     message = client.messages.create(
         body= text_reminder,
@@ -29,7 +44,9 @@ def send_reminder():
     print(f"Message sent: {message.sid}")
     storage.store_message(twilio_number, user_number, text_reminder,"scheduled_prompt",message.sid)
 
-send_reminder()
+# Call send_reminder for each user_number
+for user_number in user_numbers:
+    send_reminder(user_number)
 
 # Schedule the reminder
 schedule.every(2).hours.do(send_reminder)
