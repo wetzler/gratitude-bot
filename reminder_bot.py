01c72d9a1c02038c1c62ca5be85835d5be7f8fb8
Michelle Wetzler <1840448+wetzler@users.noreply.github.com>
2024-04-22 15:51:11 -0700

    Update reminder_bot.py
    fix issues with script sending to multiple users

--- reminder_bot.py
+++ reminder_bot.py
@@ -8,7 +8,7 @@ import requests
 
 # this stuff needs to be there for this to run on PythonAnywhere
 from dotenv import load_dotenv
-project_folder = os.path.expanduser('~/.') 
+project_folder = os.path.expanduser('~/.')
 load_dotenv(os.path.join(project_folder, '.env'))
 
 # Twilio credentials
@@ -16,12 +16,13 @@ account_sid = os.environ.get('TWILIO_ACCOUNT_SID')
 auth_token = os.environ.get('TWILIO_AUTH_TOKEN')
 twilio_number = os.environ.get('TWILIO_PHONE_NUMBER')
 
+print("regular reminders sending from: "+twilio_number)
+
 # Airtable credentials
 base_key = os.environ.get('AIRTABLE_BASE_KEY')
 table_name = os.environ.get('AIRTABLE_TABLE_NAME')
 access_token = os.environ.get('AIRTABLE_PERSONAL_ACCESS_TOKEN')
 
-print(base_key)
 # Get list of numbers that need reminders
 url = f"https://api.airtable.com/v0/{base_key}/Users"  # Define the URL to make the request to the database
 
@@ -42,22 +43,22 @@ print(user_numbers)
 # Initialize Twilio client
 client = Client(account_sid, auth_token)
 
-def send_reminder(user_number):
-    text_reminder = "hi, it's gratitude bot. what are you feeling grateful for today? 🌟"
-    message = client.messages.create(
-        body= text_reminder,
-        from_=twilio_number,
-        to=user_number
-    )
-    print(f"Message sent: {message.sid}")
-    storage.store_message(twilio_number, user_number, text_reminder,"scheduled_prompt",message.sid)
+def send_reminder(user_numbers):
+    for user_number in user_numbers:
+        text_reminder = "hi, it's gratitude bot. what are you feeling grateful for today? 🌟"
+        message = client.messages.create(
+            body= text_reminder,
+            from_=twilio_number,
+            to=user_number
+        )
+        print(f"Message sent: {message.sid}")
+        storage.store_message(twilio_number, user_number, text_reminder,"scheduled_prompt",message.sid)
 
 # Call send_reminder for each user_number
-for user_number in user_numbers:
-    send_reminder(user_number)
+send_reminder(user_numbers)
 
 # Schedule the reminder
-schedule.every(2).hours.do(send_reminder)
+schedule.every(24).hours.do(send_reminder, user_numbers)
 
 # Keep the script running
 while True:
